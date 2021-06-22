218fc0a011800f067dafc2c6a574fc238bf51f64
Rohit Kumar <lit2019037@iiitl.ac.in>
2021-06-22 16:37:50 +0530

    sending all errors through in express validator

--- functions/validator.js
+++ functions/validator.js
@@ -4,9 +4,9 @@ exports.handleValidationError = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
+    const arr = errors.errors.map((e) => ({ msg: e.msg, param: e.param }));
     return res.status(400).json({
-      error: errors.array()[0].msg,
-      errorParam: errors.array()[0].param,
+      errors: arr,
     });
   }
   next();
