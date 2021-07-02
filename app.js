f7be409636e400120ffa27233af384dff20c2f2b
Rohit Kumar <lit2019037@iiitl.ac.in>
2021-07-02 14:48:05 +0530

    added both sites in cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://bookshlf.web.app"],
   })
 );
 
