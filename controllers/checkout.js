70ff2122b4617343f64d749447c4cdaea970f08e
Rohit Kumar <lit2019037@iiitl.ac.in>
2021-07-15 01:05:54 +0530

    completed checkoutBook route

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -1,5 +1,48 @@
 const CartItems = require("../models/cartItems");
 const Books = require("../models/books");
+const mongoose = require("mongoose");
+
+exports.checkoutBook = async (req, res) => {
+  try {
+    const bookId = req.query?.bookId;
+    const purchaseQty = Number(
+      req.query?.purchaseQty ? req.query.purchaseQty : 1
+    );
+    if (!mongoose.isValidObjectId(bookId)) {
+      return res.status(400).json({ error: "Book does not exist" });
+    }
+    const book = await Books.findOne({ _id: bookId })
+      .select({
+        _id: 1,
+        title: 1,
+        MRP: 1,
+        price: 1,
+        editionYear: 1,
+        author: 1,
+        sellerName: 1,
+        qty: 1,
+        photos: { $slice: 1 },
+      })
+      .exec();
+    book._doc.bookId = book._doc._id;
+    delete book._doc._id;
+    book._doc.photo = book._doc.photos.length > 0 ? book._doc.photos[0] : "";
+    delete book._doc.photos;
+    const shippingCharges = 40 * purchaseQty;
+    const itemsSubtotal = book.price * purchaseQty;
+    const orderTotal = shippingCharges + itemsSubtotal;
+    res.json({
+      item: book,
+      totalItems: purchaseQty,
+      shippingCharges,
+      itemsSubtotal,
+      orderTotal,
+    });
+  } catch (error) {
+    console.log("Error occurred in /checkoutBook", error);
+    res.status(500).json({ error: "Some error occurred" });
+  }
+};
 
 exports.checkoutCart = async (req, res) => {
   try {
@@ -41,7 +84,7 @@ exports.checkoutCart = async (req, res) => {
     };
     res.json(obj);
   } catch (error) {
-    console.log("Error occurred in /getCartList", error);
+    console.log("Error occurred in /checkoutCart", error);
     res.status(500).json({ error: "Some error occurred" });
   }
 };
--- routes/checkout.js
+++ routes/checkout.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const check = require("express-validator");
+const { check } = require("express-validator");
 
-const { checkoutCart } = require("../controllers/checkout");
+const { checkoutCart, checkoutBook } = require("../controllers/checkout");
 const { isSignedIn } = require("../controllers/auth");
 const { handleValidationError } = require("../functions/validator");
 
 router.get("/checkoutCart", isSignedIn, checkoutCart);
+
+router.get(
+  "/checkoutBook",
+  isSignedIn,
+  [check("bookId").notEmpty().withMessage("Book id required")],
+  handleValidationError,
+  checkoutBook
+);
 
 module.exports = router;
