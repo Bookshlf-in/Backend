2a9ac9e097549b0d48e94378e6968a57380a05d2
Rohit Kumar <lit2019037@iiitl.ac.in>
2021-07-16 01:47:24 +0530

    updated reset route

--- controllers/reset.js
+++ controllers/reset.js
@@ -4,6 +4,11 @@ const Messages = require("../models/messages");
 const Newsletters = require("../models/newsletters.js");
 const WebsiteReviews = require("../models/websiteReviews");
 const EmailOtp = require("../models/emailOtps");
+const Books = require("../models/books");
+const CartItems = require("../models/cartItems");
+const WishlistItems = require("../models/wishlistItems");
+const Orders = require("../models/orders");
+const SellerProfiles = require("../models/sellerProfiles");
 
 exports.reset = async (req, res) => {
   try {
@@ -13,6 +18,11 @@ exports.reset = async (req, res) => {
     await Newsletters.deleteMany().exec();
     await WebsiteReviews.deleteMany().exec();
     await EmailOtp.deleteMany().exec();
+    await Books.deleteMany().exec();
+    await CartItems.deleteMany().exec();
+    await WishlistItems.deleteMany().exec();
+    await Orders.deleteMany().exec();
+    await SellerProfiles.deleteMany().exec();
     res.send("Reset successfull");
   } catch (error) {
     console.log("Error in /reset", error);
