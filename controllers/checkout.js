53426cd30cb648df3f800fe068aa4f16339f2d3f
Rohit Kumar <lit2019037@iiitl.ac.in>
2021-07-16 21:39:41 +0530

    added error parameter in checkout of cart, when books not available

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -50,26 +50,33 @@ exports.checkoutCart = async (req, res) => {
     let itemsSubtotal = 0;
     const cartList = await Promise.all(
       cartItems.map(async ({ _id, bookId, createdAt, purchaseQty }) => {
-        const book = await Books.findOne({ _id: bookId })
-          .select({
-            _id: 1,
-            title: 1,
-            MRP: 1,
-            price: 1,
-            editionYear: 1,
-            author: 1,
-            sellerName: 1,
-            qty: 1,
-            photos: { $slice: 1 },
-          })
-          .exec();
-        book._doc.bookId = book._doc._id;
-        delete book._doc._id;
-        book._doc.photo =
-          book._doc.photos.length > 0 ? book._doc.photos[0] : "";
-        delete book._doc.photos;
+        const book = (
+          await Books.findOne({ _id: bookId })
+            .select({
+              _id: 1,
+              title: 1,
+              MRP: 1,
+              price: 1,
+              editionYear: 1,
+              author: 1,
+              sellerName: 1,
+              qty: 1,
+              photos: { $slice: 1 },
+            })
+            .exec()
+        )?._doc;
+        book.bookId = book._id;
+        delete book._id;
+        book.photo = book.photos.length > 0 ? book.photos[0] : "";
+        delete book.photos;
         itemsSubtotal += book.price;
-        return { ...book._doc, _id, purchaseQty, createdAt };
+        const obj = { ...book, _id, purchaseQty, createdAt };
+        if (book.qty <= 0) {
+          obj.error = "Boook sold out";
+        } else if (book.qty < purchaseQty) {
+          obj.error = `Only ${book.qty} books available`;
+        }
+        return obj;
       })
     );
     const totalItems = cartList.length;
