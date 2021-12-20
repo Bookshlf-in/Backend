205444aec36bf6d919cab1dbbcabce0aa0c87ba2
Rohit Kumar <lit2019037@iiitl.ac.in>
2021-12-20 17:53:49 +0530

    added phone no validation in seller register

--- routes/seller.js
+++ routes/seller.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
+const { handleValidationError } = require("../functions/validator");
 const {
   isSignedIn,
   isSeller,
@@ -14,7 +16,13 @@ const {
   getSellerReviews,
 } = require("../controllers/seller");
 
-router.post("/sellerRegister", isSignedIn, sellerRegister);
+router.post(
+  "/sellerRegister",
+  isSignedIn,
+  [check("phoneNo").notEmpty().withMessage("Phone number is required")],
+  handleValidationError,
+  sellerRegister
+);
 
 router.get("/getSellerProfile", getAuth, getSellerAuth, getSellerProfile);
 
