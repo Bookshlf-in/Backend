64e02aadcc547878716e455158c839fc1987ba2e
Rohit Kumar <lit2019037@iiitl.ac.in>
2021-12-28 20:58:53 +0530

    chanded gc-key dir to /tmp

--- controllers/uploadFile.js
+++ controllers/uploadFile.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const { Storage } = require("@google-cloud/storage");
 
-var dir = `${__dirname}/../tmp`;
+const dir = "/tmp";
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
