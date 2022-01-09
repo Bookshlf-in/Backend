e812d7890a61d90fec228a7a6c183257bba666ea
Rohit Kumar <lit2019037@iiitl.ac.in>
2022-01-09 15:41:54 +0530

    Restored previous version of mongoose and set useCreateIndes: true

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("DB CONNECTED");
