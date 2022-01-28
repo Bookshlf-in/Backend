2c607accd0d90822c4dc3c8abe75abadac6dedef
Rohit Kumar <lit2019037@iiitl.ac.in>
2022-01-28 13:02:11 +0530

    Implemented Cache in searchTag API

--- controllers/tag.js
+++ controllers/tag.js
@@ -1,4 +1,5 @@
 const Tags = require("../models/tags");
+const { searchTagCache } = require("../functions/cache");
 
 exports.searchTag = async (req, res) => {
   try {
@@ -6,13 +7,18 @@ exports.searchTag = async (req, res) => {
     if (!tag) {
       return res.json([]);
     }
-    const result = await Tags.find({
+    let tagList = await searchTagCache.get(tag);
+    if (tagList) {
+      return res.json(tagList);
+    }
+    tagList = await Tags.find({
       tag: {
         $regex: tag,
         $options: "i",
       },
     }).select({ _id: 0, tag: 1 });
-    return res.json(result);
+    searchTagCache.set(tag, tagList);
+    return res.json(tagList);
   } catch (error) {
     console.log("Error occurred in /getTags ", error);
     res.status(500).json({ error: "Some error occurred" });
--- functions/cache.js
+++ functions/cache.js
@@ -3,6 +3,7 @@ const LRU = require("lru-cache");
 const maxAge = 1000 * 60 * 60 * 6; // 6 hours
 
 const websiteReviewCache = new LRU({ max: 1, maxAge: maxAge });
-const searchTitleCache = new LRU({ max: 1000, maxAge: maxAge });
+const searchTitleCache = new LRU({ max: 500, maxAge: maxAge });
+const searchTagCache = new LRU({ max: 250, maxAge: maxAge });
 
-module.exports = { websiteReviewCache, searchTitleCache };
+module.exports = { websiteReviewCache, searchTitleCache, searchTagCache };
