822688dfb7e60c529c02e4cdb7c39abed1fa77ab
Rohit Kumar <lit2019037@iiitl.ac.in>
2022-04-17 17:01:16 +0530

    Change verify email and password reset email template

--- email/emailTemplates.js
+++ email/emailTemplates.js
@@ -21,37 +21,23 @@ exports.welcomeEmailTemplate = () => `
 `;
 
 exports.emailVerificationEmailTemplate = (otp) => `
-<div style="font-family: Helvetica,Arial,sans-serif;min-width:200px;overflow:auto;line-height:2">
-<div style="margin:20px auto;width:70%;">
+<div>
+<div>
 <p style="font-size:1.1em">Hi,</p>
 <p>Use the following OTP to verify your email. OTP is valid for 5 minutes</p>
 <h2><strong>${otp}</strong></h2>
-<p style="font-size:0.9em;">Regards,<br />Team Bookshlf</p>
-<hr style="border:none;border-top:1px solid #eee" />
-<div style="text-align:center;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-  <p>All rights reserved &copy;2022</p>
-  <p>Bookshlf</p>
-  <p>contact us at <a href="mailto:bookshlf.in@gmail.com" style="color: #aaa">bookshlf.in@gmail.com</a>
-  </p>
-</div>
+<p style="font-size:0.9em;">Regards<br />Team Bookshlf</p>
 </div>
 </div>
 `;
 
 exports.passwordResetEmailTemplate = (otp) => `
-<div style="font-family: Helvetica,Arial,sans-serif;min-width:200px;overflow:auto;line-height:2">
-<div style="margin:20px auto;width:70%;">
+<div>
+<div>
 <p style="font-size:1.1em">Hi,</p>
-<p>Forgot your password? Use the following OTP to change your password. OTP is valid for 5 minutes.</p>
+<p>Use the following OTP to Reset your password. OTP is valid for 5 minutes</p>
 <h2><strong>${otp}</strong></h2>
-<p style="font-size:0.9em;">Regards,<br />Team Bookshlf</p>
-<hr style="border:none;border-top:1px solid #eee" />
-<div style="text-align:center;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-  <p>All rights reserved &copy;2022</p>
-  <p>Bookshlf</p>
-  <p>contact us at <a href="mailto:bookshlf.in@gmail.com" style="color: #aaa">bookshlf.in@gmail.com</a>
-  </p>
-</div>
+<p style="font-size:0.9em;">Regards<br />Team Bookshlf</p>
 </div>
 </div>
 `;
