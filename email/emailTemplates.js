036abba3d5cc87042fb50d773f2300919bf8955d
Rohit Kumar <lit2019037@iiitl.ac.in>
2022-04-17 16:35:11 +0530

    Changed email template for sign-up and forgot password

--- email/emailTemplates.js
+++ email/emailTemplates.js
@@ -22,13 +22,10 @@ exports.welcomeEmailTemplate = () => `
 
 exports.emailVerificationEmailTemplate = (otp) => `
 <div style="font-family: Helvetica,Arial,sans-serif;min-width:200px;overflow:auto;line-height:2">
-<div style="margin:20px auto;width:70%;padding:20px 0">
-<div style="border-bottom:1px solid #eee">
-  <a href="https://bookshlf.in/"><img src="https://storage.googleapis.com/bookshlf-in/static/logo/logoView.png" width="200px"/></a>
-</div>
+<div style="margin:20px auto;width:70%;">
 <p style="font-size:1.1em">Hi,</p>
-<p>Thank you for choosing Bookshlf. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
-<h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+<p>Use the following OTP to verify your email. OTP is valid for 5 minutes</p>
+<h2><strong>${otp}</strong></h2>
 <p style="font-size:0.9em;">Regards,<br />Team Bookshlf</p>
 <hr style="border:none;border-top:1px solid #eee" />
 <div style="text-align:center;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
@@ -43,13 +40,10 @@ exports.emailVerificationEmailTemplate = (otp) => `
 
 exports.passwordResetEmailTemplate = (otp) => `
 <div style="font-family: Helvetica,Arial,sans-serif;min-width:200px;overflow:auto;line-height:2">
-<div style="margin:20px auto;width:70%;padding:20px 0">
-<div style="border-bottom:1px solid #eee">
-  <a href="https://bookshlf.in/"><img src="https://storage.googleapis.com/bookshlf-in/static/logo/logoView.png" width="200px"/></a>
-</div>
+<div style="margin:20px auto;width:70%;">
 <p style="font-size:1.1em">Hi,</p>
 <p>Forgot your password? Use the following OTP to change your password. OTP is valid for 5 minutes.</p>
-<h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+<h2><strong>${otp}</strong></h2>
 <p style="font-size:0.9em;">Regards,<br />Team Bookshlf</p>
 <hr style="border:none;border-top:1px solid #eee" />
 <div style="text-align:center;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
