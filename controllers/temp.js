af161bb05745450e418518a222fc3982724369a7
Rohit Kumar <lit2019037@iiitl.ac.in>
2022-05-06 02:27:23 +0530

    temp

--- controllers/temp.js
+++ controllers/temp.js
@@ -12,7 +12,6 @@ exports.getBestSellingBooks = async (req, res) => {
       "61e7abf5f9c63c00230eb53c",
       "6165bcd5a2880d00228df884",
       "61e71445e21a97002334620d",
-      "61e7f4a1f9c63c00230ec540",
     ];
     // const bookList = [
     //   "624d4e64715eb8bc116f1fd4",
@@ -21,7 +20,6 @@ exports.getBestSellingBooks = async (req, res) => {
     //   "61ed232478169e0020c792ae",
     //   "620cb3ad4901e9000a4dc44f",
     //   "61ebcefcb72be500207f8d16",
-    //   "620764a7171b06000a58ffa4",
     // ];
     const books = await Promise.all(
       bookList.map(async (bookId) => {
@@ -76,10 +74,43 @@ exports.getBestSellingBooks = async (req, res) => {
 exports.getRecommendedBooks = async (req, res) => {
   try {
     const bookId = req.query.bookId;
-    return "lolo";
-    // const book = await Books.findOne({ _id: bookId });
-    // const tagArr = book.tags[0];
-    // const recommendedBooks = await Books.find({ tags: { $in: tagArr } });
+    const userId = req.auth?._id;
+    if (!bookId) {
+      return res.json({ error: "bookId required" });
+    }
+    const book = await Books.findOne({ _id: bookId });
+    const tagArr = book.tags[0];
+    const recommendedBooks = await Books.find({ tags: { $in: tagArr } });
+    const data = await Promise.all(
+      recommendedBooks.map(async (book) => {
+        book = book._doc;
+        book.photo = book.photos?.length > 0 ? book.photos[0] : "";
+        delete book.photos;
+        const seller = (
+          await SellerProfiles.findOne({ _id: book.sellerId }).select({
+            _id: 0,
+            rating: 1,
+          })
+        )?._doc;
+        book.sellerRating = seller.rating;
+        book.wishlist = false;
+        book.cart = false;
+        if (userId) {
+          const wishlistItem = await WishlistItems.findOne({
+            userId,
+            bookId: book._id,
+          }).select({ _id: 1 });
+          if (wishlistItem) book.wishlist = true;
+          const cartItem = await CartItems.findOne({
+            userId,
+            bookId: book._id,
+          }).select({ _id: 1 });
+          if (cartItem) book.cart = true;
+        }
+        return book;
+      })
+    );
+    res.json(data);
   } catch (error) {
     console.log("Error occurred in /getBestSellingBooks ", error);
     res.status(500).json({ error: "Some error occurred" });
--- routes/temp.js
+++ routes/temp.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const { getAuth } = require("../controllers/auth");
+
 const { getBestSellingBooks, getRecommendedBooks } = require("../controllers/temp");
 
-router.get("/getBestSellingBooks", getBestSellingBooks);
+router.get("/getBestSellingBooks", getAuth, getBestSellingBooks);
 router.get("/getUserRecommendedBooks", getBestSellingBooks);
-router.get("/getRecommendedBooks", getRecommendedBooks);
+router.get("/getRecommendedBooks", getAuth, getRecommendedBooks);
 
 module.exports = router;
